2daedc150dbf3dfdf9eee4f64ec89f8883d4cc97
梦宇 <github@plter.com>
2014-04-01 11:04:40 +0800

    完善关闭事件处理示例

--- v2.0/adt/L003ClosingActivity/src/com/plter/closingactivity/MainActivity.java
+++ v2.0/adt/L003ClosingActivity/src/com/plter/closingactivity/MainActivity.java
@@ -4,13 +4,22 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 
 import com.plter.game2d.display.Game2DActivity;
+import com.plter.game2d.display.Label;
 import com.plter.game2d.events.EventListener;
 import com.plter.game2d.events.Game2DActivityEvent;
+import com.plter.game2d.geom.Size;
 
 public class MainActivity extends Game2DActivity {
 
 	@Override
 	protected void onGame2DCreationComplete() {
+		
+		
+		Size s = getStage().getVisibleSize();
+		Label l = new Label("Please press the back button");
+		l.setPosition(s.getWidth()/2, s.getHeight()/2);
+		getStage().addChild(l);
+		
 		
 		closing.add(new EventListener<Game2DActivityEvent>() {
 			
